abb3da4bef94b833be4ede268df8256f482fe53b
zedd554 <132743361+zedd554@users.noreply.github.com>
2023-07-02 22:27:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -39,26 +39,41 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
 
 document.addEventListener("keydown", function(event) {
     if (event.key === "w") {
+ 
         tom1 = new Audio("sounds/crash.mp3");
         tom1.play();
+        document.querySelectorAll("button")[0].className="w drum pressed";
+        setTimeout(function(){document.querySelectorAll("button")[0].className="w drum";},100);
     } else if (event.key === "a") {
         tom2 = new Audio("sounds/kick-bass.mp3");
-        tom2.play();
+       tom2.play();
+      document.querySelectorAll("button")[1].className="a drum pressed";
+      setTimeout(function(){document.querySelectorAll("button")[1].className="a drum";},100);
     } else if (event.key === "s") {
         tom3 = new Audio("sounds/snare.mp3");
         tom3.play();
+      document.querySelectorAll("button")[2].className="s drum pressed";
+      setTimeout(function(){document.querySelectorAll("button")[2].className="s drum";},100);
     } else if (event.key === "d") {
         tom4 = new Audio("sounds/tom-1.mp3");
         tom4.play();
+       document.querySelectorAll("button")[3].className="d drum pressed";
+       setTimeout(function(){document.querySelectorAll("button")[3].className="d drum";},100);
     } else if (event.key === "j") {
         tom5 = new Audio("sounds/tom-2.mp3");
         tom5.play();
+       document.querySelectorAll("button")[4].className="j drump pressed";
+       setTimeout(function(){document.querySelectorAll("button")[4].className="j drum";},100);
     } else if (event.key === "k") {
         tom6 = new Audio("sounds/tom-3.mp3");
         tom6.play();
+       document.querySelectorAll("button")[5].className="k drum pressed";
+       setTimeout(function(){document.querySelectorAll("button")[5].className="k drum";},100);
     } else if (event.key === "l") {
         tom7 = new Audio("sounds/tom-1.mp3");
         tom7.play();
+      document.querySelectorAll("button")[6].className="l drum pressed";
+      setTimeout(function(){document.querySelectorAll("button")[6].className="l drum";},100);
     } else {
         alert("Press a valid key");
     }
